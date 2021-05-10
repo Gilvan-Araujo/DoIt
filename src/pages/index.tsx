bdd052ed1836a0fe20c5933f9720ec56cf3c8139
Gilvan <45008443+Gilvan-Araujo@users.noreply.github.com>
2021-05-10 16:15:49 -0300

    Finalize transition

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header } from '../components/Header'
+import { TaskList } from "../components/TaskList";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <TaskList />
+    </>
+  )
+}
